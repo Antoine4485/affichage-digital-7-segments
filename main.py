af5ccff5a77e2450f811b4968de127665e1f746a
Antoine4485 <pzx.antoine@gmail.com>
2024-08-27 12:14:10 +0200

    # utilisation dictionnaire __bin_codes_by_digits

--- main.py
+++ main.py
@@ -3,63 +3,39 @@ from datetime import datetime
 
 class ShowTime:
 
-    NB_BITS = 4
-    NB_HEXA_DIGITS_IN_DICT_HEXACODES_BY_DIGITS = 5
-    hexacodes_by_digits = {
-        "0": 0xF999F,
-        "1": 0x11111,
-        "2": 0xF1F8F,
-        "3": 0xF1F1F,
-        "4": 0x99F11,
-        "5": 0xF8F1F,
-        "6": 0xF8F9F,
-        "7": 0xF1111,
-        "8": 0xF9F9F,
-        "9": 0xF9F1F
+    __NB_BITS = 4
+    __bin_codes_by_digits = {
+        "0": ["1111", "1001", "1001", "1001", "1111"],
+        "1": ["0001", "0001", "0001", "0001", "0001"],
+        "2": ["1111", "0001", "1111", "1000", "1111"],
+        "3": ["1111", "0001", "1111", "0001", "1111"],
+        "4": ["1001", "1001", "1111", "0001", "0001"],
+        "5": ["1111", "1000", "1111", "0001", "1111"],
+        "6": ["1000", "1000", "1111", "1001", "1111"],
+        "7": ["1111", "0001", "0001", "0001", "0001"],
+        "8": ["1111", "1001", "1111", "1001", "1111"],
+        "9": ["1111", "1001", "1111", "0001", "0001"]
     }
-    bin_code_two_points = "01010"
+    __bin_code_two_points = "01010"
 
     def __init__(self, time=datetime.now()):
-        self._time = time
+        self.__time = time
 
-    @property
-    def time(self):
-        return self._time
-
-    def get_bin_code_list_by_hexa_code(self, hexa_code):
-        bin_code = self.get_bin_code_by_hexa_code(hexa_code)
-        bin_code_list_by_hexa_code = [bin_code[i:i + self.NB_BITS] for i in range(0, len(bin_code), self.NB_BITS)]
-        return bin_code_list_by_hexa_code
-
-    def get_bin_code_by_hexa_code(self, hexa_code):
-        # on transforme en code binaire et on enlève les 2 lettres au début
-        bin_code = bin(hexa_code)[2::]
-        normal_length = self.NB_HEXA_DIGITS_IN_DICT_HEXACODES_BY_DIGITS * self.NB_BITS
-        # on rajoute des "0" au début de "bin_code" dans cas où le 1er code hexa de "bin_code"
-        # commence par des 0, ces "0" ayant été supprimés automatiquement par la fonction "bin"
-        bin_code_by_hexa_code = f"{bin_code:>0{normal_length}}"
-        return bin_code_by_hexa_code
-
-    def get_bin_code_list_by_digit(self, digit):
-        hexacodes_by_digits = self.hexacodes_by_digits[digit]
-        bin_code_list_by_hexa_code = self.get_bin_code_list_by_hexa_code(hexacodes_by_digits)
-        return bin_code_list_by_hexa_code
-
-    def get_bin_code_lists_by_digits(self):
-        bin_code_lists_by_digits = [self.get_bin_code_list_by_digit(digit) for digit in str(self.time.hour)]
-        bin_code_lists_by_digits.append(list(self.bin_code_two_points))
-        bin_code_lists_by_digits.extend(self.get_bin_code_list_by_digit(digit) for digit in self.time.strftime("%M"))
+    def __get_bin_code_lists_by_digits(self):
+        bin_code_lists_by_digits = [self.__bin_codes_by_digits[digit] for digit in str(self.__time.hour)]
+        bin_code_lists_by_digits.append(list(self.__bin_code_two_points))
+        bin_code_lists_by_digits.extend(self.__bin_codes_by_digits[digit] for digit in self.__time.strftime("%M"))
         return bin_code_lists_by_digits
 
-    def show_in_command_line(self):
-        bin_code_lists_by_digits = self.get_bin_code_lists_by_digits()
+    def digital_time(self):
+        bin_code_lists_by_digits = self.__get_bin_code_lists_by_digits()
         formatted_bin_code_lists_by_digits = list(zip(*bin_code_lists_by_digits))
         separator = "  "
         drawing = ""
         white_spaces = ""
 
-        if self.time.hour < 10:
-            white_spaces = " " * self.NB_BITS + separator
+        if self.__time.hour < 10:
+            white_spaces = " " * self.__NB_BITS + separator
 
         for bin_code_list in formatted_bin_code_lists_by_digits:
             bin_code = white_spaces + separator.join(bin_code_list)
@@ -70,17 +46,16 @@ class ShowTime:
 
 if __name__ == '__main__':
 
-    date_time_list = [
-        datetime(2024, 10, 10, 10, 26, 36),
-        datetime(2024, 10, 10, 22, 56, 36),
-        datetime(2024, 10, 10, 1, 57, 36),
-        datetime(2024, 10, 10, 1, 58, 36),
-        datetime(2024, 10, 10, 1, 4, 36)
-    ]
-
-    for one_date_time in date_time_list:
-        show_time = ShowTime(one_date_time)
-        show_time.show_in_command_line()
+    # date_time_list = [
+    #     datetime(2024, 10, 10, 10, 23, 36),
+    #     datetime(2024, 10, 10, 14, 56, 36),
+    #     datetime(2024, 10, 10, 8, 57, 36),
+    #     datetime(2024, 10, 10, 0, 59, 36),
+    # ]
+    #
+    # for one_date_time in date_time_list:
+    #     show_time = ShowTime(one_date_time)
+    #     show_time.digital_time()
 
     show_time = ShowTime()
-    show_time.show_in_command_line()
+    show_time.digital_time()
